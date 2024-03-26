8b9e9fc3cc2b236787c89352954ca60d44f4d623
stefan1708 <153618198+stefan1708@users.noreply.github.com>
2024-03-26 14:58:02 +0200

    Add files via upload

---
+++ response.js
@@ -0,0 +1,9 @@
+const responseObj = {
+    buna: "Buna! Cu ce te pot ajuta?",
+    capul: "Ia un Nurofen.",
+    bila: "Ia un Colebil.",
+    inima: "Ia un Aspirin Cardio.",
+    respir: "Trebuie sa fii consultat de un specialist.",
+    data: new Date().toDateString(),
+    ceasul: new Date().toLocaleTimeString(),
+};
